59351ed310a57c3953fbd537645d82213d4d2601
Leonardo Vargas Olvera <124732244+LeonardoVargas19@users.noreply.github.com>
2024-01-13 17:31:23 -0600

    Segundo commit

---
+++ src/helloworld/Main.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo ");
+        Persona persona = new Persona("Rei Ayanami",12);
+        persona.saludar();
+    }
+}
