672a02e366221307ab0f535dec9239fe6be948f3
Leonardo Vargas Olvera <124732244+LeonardoVargas19@users.noreply.github.com>
2024-01-13 18:02:43 -0600

    CuartoCommit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import figura.Cuadrado;
+import figura.Rectangulo;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Rectangulo rectangulo = new Rectangulo(10,3);
+        System.out.println("Datos de un rectangulo");
+        System.out.println(rectangulo.calcularArea());
+        System.out.println(rectangulo.calcularPerimetro());
+
+        Cuadrado cuadrado = new Cuadrado(4);
+        System.out.println("Datos del cuadrado");
+        System.out.println(cuadrado.calcularArea());
+        System.out.println(cuadrado.calcularPerimetro());
     }
+
 }
--- src/figura/Cuadrado.java
+++ src/figura/Cuadrado.java
@@ -1,4 +1,19 @@
 package figura;
 
-public class Cuadrado {
+public class Cuadrado implements FiguraGeometrica {
+    private int lado;
+
+    @Override
+    public double calcularArea() {
+        return lado * lado;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 4 * lado;
+    }
+
+    public Cuadrado(int lado) {
+        this.lado = lado;
+    }
 }
